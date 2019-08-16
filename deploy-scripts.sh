27e410bd6e05da6e1939ab8a142a61da6687592c
gputrick <gputrick@gmail.com>
2019-08-16 16:27:21 -0300

    Corrigido zip do build do frontend

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -11,7 +11,7 @@ build-frontend()
     npm rebuild node-sass  || exit 1
     npm run-script $NPM_SCRIPT  || exit 1
     cd $BUILD_PATH  || exit 1
-    zip -r $ZIP_NAME . -i ./*  || exit 1
+    zip -r $ZIP_NAME ./*  || exit 1
     echo "---------- END BUILD FRONTEND -----------"
 }
 
@@ -37,6 +37,8 @@ send-build()
 restart-build()
 {
     echo "---------- START TO RESTART BUILD IN REMOTE ENVIROMENT -----------"
+    notify-send -i important "Build complete!" "See terminal to insert passphere"
+    paplay /usr/share/sounds/freedesktop/stereo/complete.oga
     echo $PASSPHERE  || exit 1
     ssh -i $PEM_FILE "$REMOTE_USER@$IP" $RESTART_COMAND  || exit 1
     echo "---------- END RESTART BUILD IN REMOTE ENVIROMENT -----------"
@@ -45,5 +47,5 @@ restart-build()
 #LOGGING
 LOG_FILE="$BASEDIR/$ENVIRONMENT_NAME.log"
 > $LOG_FILE #clear log file
-exec 1> >(awk -W interactive "{\"date\" | getline d; print \$0; print d, \$0 >> \"$LOG_FILE\"}")
+exec 1> >(mawk -W interactive "{\"date\" | getline d; print \$0; print d, \$0 >> \"$LOG_FILE\"}")
 exec 2>&1
